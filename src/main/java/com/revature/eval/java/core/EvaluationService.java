51b8197a5ac91553434f6f6b684d31aa2824bad4
Austen <austen.schips@gmail.com>
2018-08-20 16:06:47 -0400

    Second Update

--- src/main/java/com/revature/eval/java/core/EvaluationService.java
+++ src/main/java/com/revature/eval/java/core/EvaluationService.java
@@ -629,7 +629,43 @@ public class EvaluationService {
 	 */
 	public boolean isValidIsbn(String string) {
 		// TODO Write an implementation for this method declaration
-		return false;
+		
+		 if ( string == null )
+	        {
+	            return false;
+	        }
+
+	        //removing all hyphens
+	        string = string.replaceAll( "-", "" );
+
+	        //must be a 10 digit ISBN
+	        if ( string.length() != 10 )
+	        {
+	            return false;
+	        }
+
+	        try
+	        {
+	            int tot = 0;
+	            for ( int i = 0; i < 9; i++ )
+	            {
+	                int digit = Integer.parseInt( string.substring( i, i + 1 ) );
+	                tot += ((10 - i) * digit);
+	            }
+
+	            String checksum = Integer.toString( (11 - (tot % 11)) % 11 );
+	            if ( "10".equals( checksum ) )
+	            {
+	                checksum = "X";
+	            }
+
+	            return checksum.equals( string.substring( 9 ) );
+	        }
+	        catch ( NumberFormatException nfe )
+	        {
+	            //to catch invalid ISBNs that have non-numeric characters in them
+	            return false;
+	        }
 	}
 
 	/**
@@ -647,7 +683,37 @@ public class EvaluationService {
 	 */
 	public boolean isPangram(String string) {
 		// TODO Write an implementation for this method declaration
-		return false;
+		  boolean[] mark = new boolean[26];
+		  
+	        
+	        int index = 0;
+	        
+	        for (int i = 0; i < string.length(); i++)
+	        {
+	            // If uppercase character, subtract 'A' to find index.
+	            if ('A' <= string.charAt(i) && 
+	                    string.charAt(i) <= 'Z')
+	                         
+	                index = string.charAt(i) - 'A';
+	 
+	                // If lowercase character, subtract 'a' to find index.
+	            else if('a' <= string.charAt(i) && 
+	                        string.charAt(i) <= 'z')
+	                             
+	                index = string.charAt(i) - 'a';
+	 
+	            // Mark current character
+	            mark[index] = true;
+	        }
+	 
+	        // Return false if any character is unmarked
+	        for (int i = 0; i <= 25; i++)
+	            if (mark[i] == false)
+	                return (false);
+	 
+	        // If all characters were present
+	        return (true);
+		
 	}
 
 	/**
@@ -660,6 +726,7 @@ public class EvaluationService {
 	 */
 	public Temporal getGigasecondDate(Temporal given) {
 		// TODO Write an implementation for this method declaration
+		
 		return null;
 	}
 
